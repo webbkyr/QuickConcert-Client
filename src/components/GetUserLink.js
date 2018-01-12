42643663dc01dcf6c01572786a7fcc8bc3197b65
Kayla Webb <kaylarwebb@gmail.com>
2018-01-12 08:46:45 -0500

    Fix trpo in unsused component, try travis build again

--- src/components/GetUserLink.js
+++ src/components/GetUserLink.js
@@ -1,37 +1,32 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import './usereventdetails-view.css';
-import { fetchLinkSuccess } from '../actions/eventlinks';
-// import UserEventDetails from './usereventdetails'
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import './usereventdetails-view.css';
+// import { fetchLinkSuccess } from '../actions/eventlinks';
+// // import UserEventDetails from './usereventdetails'
 
-// { __v: 0,
-//   eventName: 'Thrillkiller',
-//   creator: 'Kim',
-//   _id: 5a4f7d804db91b62428166ff,
-//   attendees: [ { attendee: 'Kim', _id: 5a4f7d804db91b6242816700 } ] }
-
-
-export class UserConcertDetails extends React.Component {
-
-  render() {
-    return (    
-      <div>
-        <UserEventDetails />
-        {/* {this.renderDetails} */}
-      </div>
-    )
-  }
-}
+// // { __v: 0,
+// //   eventName: 'Thrillkiller',
+// //   creator: 'Kim',
+// //   _id: 5a4f7d804db91b62428166ff,
+// //   attendees: [ { attendee: 'Kim', _id: 5a4f7d804db91b6242816700 } ] }
 
 
+// export function GetUserLink(props) {
+
+//     return (    
+//       <div>
+//         <UserEventDetails />
+//         {/* {this.renderDetails} */}
+//       </div>
+//     )
+//   }
+// }
 
 
+// export const mapStateToProps = state => {
+//   return {
+//     eventLink: state.eventLink
+//   }
+// }
 
-
-export const mapStateToProps = state => {
-  return {
-    eventLink: state.eventLink
-  }
-}
-
-export default connect(mapStateToProps)(UserEventDetailsView)
+// export default connect(mapStateToProps)(GetUserLink)
